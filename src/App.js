4c8d65760ac72d49188bab55418fed384e1b17a4
iluha <iluha@iluha.com>
2018-11-22 10:45:33 +0300

    Install main dependencies

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from "react-router-redux";
+import store from "./store";
+import history from './history';
+import {NavLink} from "react-router-dom";
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    <div>
+                        <h2>Main menu</h2>
+                        <div><NavLink activeStyle={{color: 'red'}} to="/counter">Counter</NavLink></div>
+                        <div><NavLink activeStyle={{color: 'red'}} to="/filters">Filters</NavLink></div>
+                        <div><NavLink activeStyle={{color: 'red'}} to="/articles">Articles</NavLink></div>
+                    </div>
+                </ConnectedRouter>
+            </Provider>
+        );
+    }
 }
 
 export default App;
---
+++ src/store.js
@@ -0,0 +1,15 @@
+import {createStore, applyMiddleware} from 'redux';
+import thunk from "redux-thunk";
+import {routerMiddleware} from 'react-router-redux';
+import history from './history';
+import reducer from './Reducers'
+
+
+const enhancer = applyMiddleware(thunk, routerMiddleware(history));
+
+const store = createStore(reducer, {}, enhancer);
+
+//dev only
+window.store = store;
+
+export default store;
