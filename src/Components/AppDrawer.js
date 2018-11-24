9f21210c6ba07347ff4ce2ee5e4d853667b6de6b
iluha <iluha@iluha.com>
2018-11-25 00:22:52 +0300

    add AppWrawer with Bar

---
+++ src/Components/AppDrawer.js
@@ -0,0 +1,217 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Hidden from '@material-ui/core/Hidden';
+import Divider from '@material-ui/core/Divider';
+import MenuIcon from '@material-ui/icons/Menu';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MailIcon from '@material-ui/icons/Mail';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        // flexGrow: 1,
+    },
+    drawer: {
+        [theme.breakpoints.up('md')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    appBar: {
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('md')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+    },
+    menuButton: {
+        marginRight: 20,
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+    rightMenuButton: {
+        flexGrow: '1',
+        justifyContent: 'flex-end',
+        display: 'flex'
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+});
+
+class ResponsiveDrawer extends React.Component {
+    state = {
+        mobileOpen: false,
+        anchorEl: null,
+    };
+
+    handleDrawerToggle = () => {
+        this.setState(state => ({mobileOpen: !state.mobileOpen}));
+    };
+
+    handleMenu = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    render() {
+        const {classes, theme} = this.props;
+        const {anchorEl} = this.state;
+        const open = Boolean(anchorEl);
+
+
+        const drawer = (
+            <div>
+                <div className={classes.toolbar}/>
+                <Divider/>
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider/>
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+        );
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline/>
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerToggle}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Панель скоркипера
+                        </Typography>
+                        {(
+                            <div className={classes.rightMenuButton}>
+                                <IconButton
+                                    aria-owns={open ? 'menu-appbar' : undefined}
+                                    aria-haspopup="true"
+                                    onClick={this.handleMenu}
+                                    color="inherit"
+                                    // className={classes.rightMenuButton}
+                                >
+                                    <AccountCircle/>
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={open}
+                                    onClose={this.handleClose}
+                                >
+                                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                                </Menu>
+                            </div>
+                        )}
+                    </Toolbar>
+                    {/*<Toolbar>*/}
+                        {/*<IconButton*/}
+                            {/*color="inherit"*/}
+                            {/*aria-label="Open drawer"*/}
+                            {/*onClick={this.handleDrawerToggle}*/}
+                            {/*className={classes.menuButton}*/}
+                        {/*>*/}
+                            {/*<MenuIcon/>*/}
+                        {/*</IconButton>*/}
+                        {/*<Typography variant="h6" color="inherit" noWrap>*/}
+                            {/*Responsive drawer*/}
+                        {/*</Typography>*/}
+                    {/*</Toolbar>*/}
+                </AppBar>
+                <nav className={classes.drawer}>
+                    {/* The implementation can be swap with js to avoid SEO duplication of links. */}
+                    <Hidden mdUp implementation="css">
+                        <Drawer
+                            container={this.props.container}
+                            variant="temporary"
+                            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                            open={this.state.mobileOpen}
+                            onClose={this.handleDrawerToggle}
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                            ModalProps={{
+                                keepMounted: true, // Better open performance on mobile.
+                            }}
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                    <Hidden smDown implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper,
+                            }}
+                            variant="permanent"
+                            open
+                        >
+                            {drawer}
+                        </Drawer>
+                    </Hidden>
+                </nav>
+            </div>
+        );
+    }
+}
+
+ResponsiveDrawer.propTypes = {
+    classes: PropTypes.object.isRequired,
+    // Injected by the documentation to work in an iframe.
+    // You won't need it on your project.
+    container: PropTypes.object,
+    theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, {withTheme: true})(ResponsiveDrawer);
--- src/Components/Scorekeeper.js
+++ src/Components/Scorekeeper.js
@@ -1,20 +1,48 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from "react-redux";
+import Bar from "./Bar";
+import AppDrawer from "./AppDrawer";
+import {withStyles} from "@material-ui/core/styles/index";
+import Typography from '@material-ui/core/Typography';
+
 
 class Scorekeeper extends Component {
     render() {
+        const { classes } = this.props;
+
         return (
             <div>
-                привет, {this.props.user.name}
+                {/*<Bar />*/}
+                <AppDrawer />
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <Typography paragraph>
+                        привет, {this.props.user.name}
+                    </Typography>
+                </main>
+
             </div>
         );
     }
 }
 
 Scorekeeper.propTypes = {
-    user: PropTypes.object.isRequired
+    user: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+});
+
 
 const mapStateToProps = (state) => {
     return {
@@ -26,4 +54,4 @@ const mapDispatchToProps = () => {
     return {};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Scorekeeper);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(Scorekeeper));
